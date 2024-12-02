806fe9d0b80d3fb66890241a5b0eb9a41ffa2ef7
hannasamuel20 <hanna17st@gmail.com>
2024-12-02 22:17:18 +0300

    change doc

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 # Load model
-model = joblib.load('../model/gradientBoostingClassifier.pkl')
+model = joblib.load('./model/gradientBoostingClassifier.pkl')
 
 
 # App title
@@ -13,26 +13,12 @@ st.subheader("Sample Data")
 st.write(
     pd.DataFrame(
         {
-            "Country": ["Australia"],
-            "Year": [2008],
-            "Status": ["Developed"],
-            "Life expectancy": [81.3],
-            "Adult Mortality": [66.0],
-            "Alcohol": [10.76],
-            "percentage expenditure": [8547.292357],
-            "Hepatitis B": [94.0],
-            "Measles": [65],
-            "BMI": [62.9],
-            "under-five deaths": [1],
-            "Polio": [92.0],
-            "Total expenditure": [8.78],
-            "Diphtheria": [92.0],
-            "HIV/AIDS": [0.1],
-            "GDP": [49664.6854],
-            "Population": [212492.0],
-            "thinness 1-19 years": [0.7],
-            "Income composition of resources": [0.921],
-            "Schooling": [19.1],
+            "Study_Hours_Per_Day": [5.5],
+            "Extracurricular_Hours_Per_Day": [1.8],
+            "Sleep_Hours_Per_Day": [7.7],
+            "Social_Hours_Per_Day": [1.5],
+            "Physical_Activity_Hours_Per_Day": [7.5],
+            "GPA": [3.05],
         }
     )
 )
@@ -43,26 +29,33 @@ st.write("Enter features:")
 
 study_hours_per_day = st.slider(label="Study Hours Per Day", min_value=0.0,
                                 max_value=24.0,
-                                value=5.6, step=0.1)
+                                value=5.5, step=0.1)
 
 extracurricular_hours_per_day = st.slider(label="Extracurricular Hours Per Day", min_value=0.0,
                                           max_value=24.0,
-                                          value=7.6, step=0.1)
+                                          value=1.8, step=0.1)
 
 sleep_hours_per_day = st.slider(label="Sleep Hours Per Day", min_value=0.0,
                                 max_value=24.0,
-                                value=3.6, step=0.1)
+                                value=7.7, step=0.1)
+social_hours_per_day = st.slider(label="Social Hours Per Day", min_value=0.0,
+                                 max_value=24.0,
+                                 value=1.5, step=0.1)
 
 physical_activities_hours_per_day = st.slider(label="Physical Activities Hours Per Day", min_value=0.0,
                                               max_value=24.0,
-                                              value=7.6, step=0.1)
+                                              value=7.5, step=0.1)
 gpa = st.slider(label="GPA", min_value=0.0,
                 max_value=4.0,
-                value=3.6, step=0.1)
+                value=3.05, step=0.1)
 
 if st.button("Predict"):
-
     input_data = np.array([study_hours_per_day, extracurricular_hours_per_day,
-                                    sleep_hours_per_day, physical_activities_hours_per_day, gpa]).reshape(1, -1)
+                           sleep_hours_per_day, social_hours_per_day, physical_activities_hours_per_day, gpa]).reshape(1, -1)
     prediction = model.predict(input_data)
-    st.write(f"Prediction: {prediction[0]}")
+    if prediction[0] == 0:
+        st.write("Prediction: Low")
+    elif prediction[0] == 1:
+        st.write("Prediction: Moderate")
+    else:
+        st.write("Prediction: High")
